0e1ffd30d625f794a50d11fc8644308613b86d1e
Nicki <prznico@gmail.com>
2025-09-25 18:34:42 -0600

    Se creó la capeta src/funciones.php y se modificó el index.php

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -3,24 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 7</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
-        }
+        include 'src/funciones.php';
     ?>
 
     <h2>Ejemplo de POST</h2>
