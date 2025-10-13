b713addef08afcc2800b7c57c0449863dd7e9b65
Nicki <prznico@gmail.com>
2025-10-13 16:52:22 -0600

    Se realizó el punto 3 de la practica

---
+++ practicas/p10/get_productos_xhtml_v2.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+	<?php
+	if(isset($_GET['tope']))
+    {
+		$tope = $_GET['tope'];
+    }
+    else
+    {
+        die('Parámetro "tope" no detectado...');
+    }
+
+    // Validar que tope sea numérico
+	if (!is_numeric($tope)) {
+		die('El parámetro "tope" debe ser numérico');
+	}
+
+    $productos = [];
+
+	if (!empty($tope))
+	{
+		/** SE CREA EL OBJETO DE CONEXION */
+		@$link = new mysqli('localhost', 'root', 'N1n1c0l3.', 'marketzone');	
+
+		/** comprobar la conexión */
+		if ($link->connect_errno) 
+		{
+			die('Falló la conexión: '.$link->connect_error.'<br/>');
+			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+		}
+
+		/** Crear una tabla que no devuelve un conjunto de resultados */
+		$stmt = $link->prepare("SELECT * FROM productos WHERE unidades <= ?");
+		$stmt->bind_param("i", $tope);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		/** Obtener todos los productos */
+		while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+			$productos[] = $row;
+		}
+
+		$result->free();
+		$stmt->close();
+		$link->close();
+	}
+	?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Productos</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<h3>PRODUCTOS - UNIDADES <= <?= $tope ?></h3>
+
+			<br/>
+			
+			<?php if( count($productos) > 0 ) : ?>
+
+				<table class="table table-striped">
+					<thead class="thead-dark">
+						<tr>
+						<th scope="col">#</th>
+						<th scope="col">Nombre</th>
+						<th scope="col">Marca</th>
+						<th scope="col">Modelo</th>
+						<th scope="col">Precio</th>
+						<th scope="col">Unidades</th>
+						<th scope="col">Detalles</th>
+						<th scope="col">Imagen</th>
+						<th scope="col">Acciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach($productos as $row): ?>
+							<tr id="row-<?= $row['id'] ?>">
+								<th scope="row"><?= $row['id'] ?></th>
+								<td class="row-data"><?= $row['nombre'] ?></td>
+								<td class="row-data"><?= $row['marca'] ?></td>
+								<td class="row-data"><?= $row['modelo'] ?></td>
+								<td class="row-data">$<?= number_format($row['precio'], 2) ?></td>
+								<td class="row-data"><?= $row['unidades'] ?></td>
+								<td class="row-data"><?= utf8_encode($row['detalles']) ?></td>
+								<td><img src="<?= $row['imagen'] ?>" width="50" height="50"></td>
+								<td>
+									<button class="btn btn-warning btn-sm" onclick="editarProducto(<?= $row['id'] ?>)">Editar</button>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					</tbody>
+				</table>
+
+			<?php else : ?>
+
+				<div class="alert alert-warning">
+					No se encontraron productos con <?= $tope ?> unidades o menos.
+				</div>
+
+			<?php endif; ?>
+		</div>
+
+		<script>
+			function editarProducto(id) {
+				// Obtener todos los datos de la fila
+				var row = document.getElementById('row-' + id);
+				var data = row.querySelectorAll('.row-data');
+				
+				var nombre = data[0].innerHTML;
+				var marca = data[1].innerHTML;
+				var modelo = data[2].innerHTML;
+				var precio = data[3].innerHTML.replace('$', '').replace(',', '');
+				var unidades = data[4].innerHTML;
+				var detalles = data[5].innerHTML;
+				var imagen = row.querySelector('img').src;
+				
+				// Guardar datos en sessionStorage
+				sessionStorage.setItem('edit_id', id);
+				sessionStorage.setItem('edit_nombre', nombre);
+				sessionStorage.setItem('edit_marca', marca);
+				sessionStorage.setItem('edit_modelo', modelo);
+				sessionStorage.setItem('edit_precio', precio);
+				sessionStorage.setItem('edit_unidades', unidades);
+				sessionStorage.setItem('edit_detalles', detalles);
+				sessionStorage.setItem('edit_imagen', imagen);
+				sessionStorage.setItem('edit_accion', 'editar');
+				
+				// Redirigir al formulario
+				window.location.href = 'formulario_productos_v2.html';
+			}
+		</script>
+	</body>
+</html>
